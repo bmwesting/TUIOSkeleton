5ef0ae09301da53798753f1ab15d6ffcaabc7940
Brandt Westing <bmwesting@gmail.com>
2011-10-24 15:06:55 -0500

    Vector tracking works with both hands. Confidence tracking enabled. Clicking state enabled.

--- SkeletonTracker.cpp
+++ SkeletonTracker.cpp
@@ -100,15 +100,11 @@ void SkeletonTracker::updateHands()
     sensor_->getHandPoints(joints);
     sensor_->getShoulderPoints(joints+2);
     
-    //printf("Left Hand: %f, %f, %f.\n", joints[0].x_, joints[0].y_, joints[0].z_);
-    //printf("Right Hand: %f, %f, %f.\n", joints[1].x_, joints[1].y_, joints[1].z_);
-
     // calculate distances
     int distanceLeft, distanceRight;
     distanceLeft  = joints[2].z_ - joints[0].z_;
     distanceRight = joints[3].z_ - joints[1].z_;
     
-    //printf("Right hand Z: %f, right rear Z: %f. Difference: %d\n", joints[1].z_, joints[3].z_, distanceRight);
     
     bool confidenceLeft = TRUE;
     bool confidenceRight = TRUE;
@@ -159,9 +155,7 @@ void SkeletonTracker::updateVectors()
     Point joints[3];
     sensor_->getHandPoints(joints);
     sensor_->getHeadPoint(joints+2);
-    
-    
-    
+        
     bool confidenceLeft = true;
     bool confidenceRight = true;
     
@@ -172,14 +166,20 @@ void SkeletonTracker::updateVectors()
             confidenceLeft = false;
         if (joints[1].confidence_ <= 0.5)
             confidenceRight = false;
-        if (joints[3].confidence_ <= 0.5)
+        if (joints[2].confidence_ <= 0.5)
             confidenceLeft = confidenceRight = false;
     }
     
-    O_ks_ = SkeletonVector(-670, 820, 0);
-    screenBBoxBottom_ = SkeletonVector(515, 195, 0);
+    float distanceLeft, distanceRight;
+    distanceLeft = joints[2].z_ - joints[0].z_;
+    distanceRight = joints[2].z_ - joints[1].z_;
     
-    // have we calibrated yet?
+    O_ks_ = SkeletonVector(-1000, 1850, 0);
+    screenBBoxBottom_ = SkeletonVector(1100, 450, 0);
+    
+    /* 
+     * We are not doing auto-calibration just yet:: future!
+     *
     if(!vectorCalibrationTop_)
     {
         // check to see if we are in calibration pose for top
@@ -206,26 +206,95 @@ void SkeletonTracker::updateVectors()
         //calibrateBot();
         return;
     }
+    */
     
-    if (!confidenceLeft)
+    // we are not confident or not 'clicking' - remove cursor if exists
+    if (!confidenceLeft || distanceLeft < threshold_)
     {
+        touchServer_->removeLeftCursor();
+    }
+    if (!confidenceRight || distanceRight < threshold_)
+    {
+        touchServer_->removeRightCursor();
+    }
+    
+    // we are confident in vector position and in 'clicking' state
+    if (confidenceLeft && distanceLeft >= threshold_)
+    {
+        /* calculate the intersection of the vector formed by the user's head and hand
+         *
+         * p1 = head; p2 = hand; t0 is vector p2 - p1; c*t0 scaled the vector to the display
+         * Note: assumes screen is at z = 0, or that the sensor is located in same z plane
+         *
+         */
+         
         SkeletonVector p1_l, p2_l, p3_l, t0_l;
         p1_l = joints[2];
         p2_l = joints[0];
         
         t0_l = p2_l - p1_l;
         
+        // calculate scale factor scaleC by known Z of screen and hand vector
         float scaleC_l = -(p1_l.getPoint().z_)/(t0_l.getPoint().z_);
+        
+        // p3 is projected point on display
         p3_l = p1_l + t0_l * scaleC_l;
         
-        p3_l.print();
+        //p3_l.print();
         
-        float cursorX, cursorY;
+        // The TUIO cursors based on the projected vector, will be normalized 0.0 - 1.0
+        float cursorX_l, cursorY_l;
         
-        cursorX = (p3_l.getPoint().x_ - O_ks_.getPoint().x_)/(screenBBoxBottom_.getPoint().x_ - O_ks_.getPoint().x_);
-        cursorY = -(p3_l.getPoint().y_ - O_ks_.getPoint().y_)/(O_ks_.getPoint().y_ - screenBBoxBottom_.getPoint().y_);
-        
-        printf("CursorX: %f, CursorY: %f\n", cursorX, cursorY);
+        /*
+         * Normalization to screen coordinates, where O_ks_ is vector from sensor to screen top-left
+         * screenBBoxBottom is vector from sensor to screen bottom right
+         */
+         
+        cursorX_l = (p3_l.getPoint().x_ - O_ks_.getPoint().x_)/(screenBBoxBottom_.getPoint().x_ - O_ks_.getPoint().x_);
+        cursorY_l = -(p3_l.getPoint().y_ - O_ks_.getPoint().y_)/(O_ks_.getPoint().y_ - screenBBoxBottom_.getPoint().y_);
+                
+        // cursor is outside display bounding box
+        if ( cursorX_l < 0.0 || cursorX_l > 1.0 || cursorY_l < 0.0 || cursorY_l > 1.0)
+        {
+            touchServer_->removeLeftCursor();
+        }
+        // cursor is inside display area
+        else
+            touchServer_->updateLeftCursor(cursorX_l, cursorY_l);        
     }
+    if (confidenceRight && distanceRight >= threshold_)
+    {
+        
+        SkeletonVector p1_r, p2_r, p3_r, t0_r;
+        p1_r = joints[2];
+        p2_r = joints[1];
+        
+        t0_r = p2_r - p1_r;
+        
+        // calculate scale factor scaleC by known Z of screen and hand vector
+        float scaleC_r = -(p1_r.getPoint().z_)/(t0_r.getPoint().z_);
+        
+        // p3 is projected point on display
+        p3_r = p1_r + t0_r * scaleC_r;
+        
+                
+        // The TUIO cursors based on the projected vector, will be normalized 0.0 - 1.0
+        float cursorX_r, cursorY_r;
+        
+        cursorX_r = (p3_r.getPoint().x_ - O_ks_.getPoint().x_)/(screenBBoxBottom_.getPoint().x_ - O_ks_.getPoint().x_);
+        cursorY_r = -(p3_r.getPoint().y_ - O_ks_.getPoint().y_)/(O_ks_.getPoint().y_ - screenBBoxBottom_.getPoint().y_);
+                
+        // cursor is outside display bounding box
+        if ( cursorX_r < 0.0 || cursorX_r > 1.0 || cursorY_r < 0.0 || cursorY_r > 1.0)
+        {
+            touchServer_->removeRightCursor();
+        }
+        // cursor is inside display area
+        else
+            touchServer_->updateRightCursor(cursorX_r, cursorY_r);        
+    }
+    
+    touchServer_->commitFrame();    
+
     
 }
